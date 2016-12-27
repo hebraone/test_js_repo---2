dd87410e442933a7349d04b3c50ec02869b46529
Piotr Hebrowski <piotr.hebrowski@wp.pl>
2016-12-27 22:11:48 +0100

    pl to eng

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function triangleArea(){
-	var a = document.getElementById("podstawa").value;
-	var h = document.getElementById("wysokosc").value;
+	var a = document.getElementById("base").value;
+	var h = document.getElementById("height").value;
 	var area = a*h/2
 	document.getElementById('x').innerHTML = area;
 }
