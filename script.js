72aa3dcdf50b57f6b4edabeba0488cdc0ead3a33
Piotr Hebrowski <piotr.hebrowski@wp.pl>
2016-12-30 20:45:07 +0100

    fix var

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function triangleArea(){
 	var a = document.getElementById("base").value;
-	var h = document.getElementById("height").value;
-	var area = a*h/2
+        h = document.getElementById("height").value;
+        area = a*h/2
 	document.getElementById('x').innerHTML = area;
 }
